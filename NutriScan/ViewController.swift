7e992bb3010076e08bcb829ae495a478e4c15e20
slam0031 <slam0031@student.monash.edu>
2025-09-25 15:29:38 +1000

    Initial Commit

---
+++ NutriScan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NutriScan
+//
+//  Created by Raymond on 25/9/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
