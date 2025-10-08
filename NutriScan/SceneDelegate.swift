6be515254aaa691ddc3f93caaafaaf27cf9f2c24
slam0031 <slam0031@student.monash.edu>
2025-10-09 03:16:07 +1100

    Implemented methods to show main app or login screen based on FirebaseAuth status.

--- NutriScan/SceneDelegate.swift
+++ NutriScan/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -16,7 +17,36 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        checkAuthenticationStatus()
+    }
+    
+    func checkAuthenticationStatus() {
+        if Auth.auth().currentUser != nil {
+            // User is signed in, show main app
+            showMainApp()
+        } else {
+            // No user signed in, show login
+            showLoginScreen()
+        }
+    }
+    
+    func showMainApp() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let tabBarController = storyboard.instantiateInitialViewController() {
+            window?.rootViewController = tabBarController
+            window?.makeKeyAndVisible()
+        }
+    }
+    
+    func showLoginScreen() {
+        let loginVC = LoginViewController()
+        let navController = UINavigationController(rootViewController: loginVC)
+        navController.modalPresentationStyle = .fullScreen
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
